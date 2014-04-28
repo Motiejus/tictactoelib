33c95618592be0c1728438879d24b91bcda3045d
Motiejus Jakštys <motiejus.jakstys@spilgames.com>
2014-04-28 10:33:46 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ install_reqs = list(parse_requirements(reqs_file))
 
 setup(
     name='tictactoelib',
-    version='0.2.2',
+    version='0.2.3',
     author="Motiejus Jakštys",
     author_email='desired.mta@gmail.com',
     description="Ultimate Tic Tac Toe game logic (Lua and Python bindings)",
